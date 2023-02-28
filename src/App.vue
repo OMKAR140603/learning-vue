ad7fb7e0ee2751ecd428848b2de7130b3508b413
OMKAR140603 <81954092+OMKAR140603@users.noreply.github.com>
2023-02-28 15:50:07 +0530

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
 <img v-on:mousemove="func3()" alt="Vue logo" src="./assets/logo.png">
- <!-- <HelloWorld msge="Welcome to Your Vue.js App"/> 
+ <HelloWorld msge="Welcome to Your Vue.js App"/> 
 <HomeComponent></HomeComponent>
 <countCompo ></countCompo>
 <toggleComponent></toggleComponent>
@@ -13,61 +13,61 @@
 <loginCompo></loginCompo>
 <slotsParent></slotsParent>
 <dynamicCompo></dynamicCompo>
-<homeCompo></homeCompo> -->
-<!-- <beforeCreate></beforeCreate> -->
-<!-- <beforeMount></beforeMount> -->
-<!-- <landingComponent></landingComponent> -->
-<!-- <getApi></getApi> -->
+<homeCompo></homeCompo>
+<beforeCreate></beforeCreate>
+<beforeMount></beforeMount>
+<landingComponent></landingComponent>
+ <getApi></getApi>
 <postApi></postApi>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
-// import HomeComponent from './components/Home.vue'
-// import countCompo from './components/countinc.vue'
-// import toggleComponent from './components/toggle.vue'
-// import forLoop from './components/forloop.vue'
-// import parentCompo from './components/parentCompo.vue'
-// import htmlBinding from './htmlBinding.vue'
-// import cardInfo from './components/cardInfo.vue'
-// import refInfo from './components/refInfo.vue';
-// import loginCompo from './loginCompo.vue';
-// import slotsParent from './components/slotsParent.vue';
-// import dynamicCompo from './components/DynamicComponents/mainCompo.vue'
-// import homeCompo from './components/footerComponents/homeCompo.vue';
-// import beforeCreate from './components/LifeCycle/beforeCreate.vue';
-// import beforeMount from './components/LifeCycle/beforeMount.vue';
-// import landingComponent from "./components/routingComponent.vue/landingComponent.vue"
-// import getApi from './components/API/getApi.vue';
+import HelloWorld from './components/HelloWorld.vue'
+import HomeComponent from './components/Home.vue'
+import countCompo from './components/countinc.vue'
+import toggleComponent from './components/toggle.vue'
+import forLoop from './components/forloop.vue'
+import parentCompo from './components/parentCompo.vue'
+import htmlBinding from './htmlBinding.vue'
+import cardInfo from './components/cardInfo.vue'
+import refInfo from './components/refInfo.vue';
+import loginCompo from './loginCompo.vue';
+import slotsParent from './components/slotsParent.vue';
+import dynamicCompo from './components/DynamicComponents/mainCompo.vue'
+import homeCompo from './components/footerComponents/homeCompo.vue';
+import beforeCreate from './components/LifeCycle/beforeCreate.vue';
+import beforeMount from './components/LifeCycle/beforeMount.vue';
+import landingComponent from "./components/routingComponent.vue/landingComponent.vue"
+import getApi from './components/API/getApi.vue';
 import postApi from "./components/API/postApi.vue"
 export default {
     name: 'App',
     components: {
-//         HelloWorld,
-//         HomeComponent,
-//         countCompo ,
-//         toggleComponent,
-//         forLoop,
-//         parentCompo,
-//         htmlBinding,
-//         cardInfo,
-//          refInfo,
-//         loginCompo,
+        HelloWorld,
+        HomeComponent,
+        countCompo ,
+        toggleComponent,
+        forLoop,
+        parentCompo,
+        htmlBinding,
+        cardInfo,
+         refInfo,
+        loginCompo,
        
-//         slotsParent,
-//         dynamicCompo,
-//         homeCompo,
-// beforeCreate,
-// beforeMount
-// landingComponent
-// getApi
-postApi
-    }
-//     methods: {
-//         func3() {
-//             console.warn("image hovered");
-//         }
-//     },
+        slotsParent,
+        dynamicCompo,
+        homeCompo,
+beforeCreate,
+beforeMount,
+landingComponent,
+getApi,
+postApi,
+    },
+    methods: {
+        func3() {
+            console.warn("image hovered");
+        }
+    },
 
 }
 </script>
